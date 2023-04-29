7d90bf113c456209a3777c3db5a0205f8db12278
burak48 <buraksaglam3@gmail.com>
2023-04-29 15:15:58 +0300

    blog-list mock data generated

---
+++ frontend/src/components/BlogList/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function BlogList() {
+    return (
+        <div>
+            <h2>BlogList</h2>
+            <p>This is BlogList page</p>
+        </div>
+    )
+}
+
+export default BlogList
